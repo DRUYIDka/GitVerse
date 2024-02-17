ccf8592f5e0ab5fdcc4eff3a9cea68f14cff5dab
admin <admin@MacBook-Pro-admin.local>
2024-02-17 22:40:17 +0300

    Initial Commit

---
+++ wordcoin3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordcoin3
+//
+//  Created by admin on 17.02.2024.
+//  Copyright © 2024 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
